989543602b7d4bc78bb462850fa3f208bf7c99fc
bohdan <bohdanstep23@gmail.com>
2023-04-08 13:14:01 +0300

    gen pass - Done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,7 @@
+   #creating pass gen
 
+# Combining the character types requested by the user
+characters = letters + numbers + special_chars
+
+# Generating the password
+password = "".join(random.choice(characters) for i in range(password_length))
